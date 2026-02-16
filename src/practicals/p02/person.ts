889d1b3a6fff0047e78d68d8addc14251d260ebb
zdvfxdvfvfvfzvz <68010756@kmitl.ac.th>
2026-02-16 09:57:37 +0700

    second push. test doesnt check

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,22 @@
-export class Person {}
+export class Person {
+    firstname: string
+    lastname: string
+    private age: number
+    static COUNTRY = 'Thailand'
+
+    constructor() {
+        this.firstname = ''
+        this.lastname = ''
+        this.age = 0
+    }
+
+    getFullName(): string {
+        return this.firstname + ' ' + this.lastname;
+    }
+    getAge(): number {
+        return this.age;
+    }
+    setAge(age :number): void {
+        this.age = age;
+    }
+}
