5aca3d98931a31d2dd0be29bec8c8c0089f07c15
zdvfxdvfvfvfzvz <68010756@kmitl.ac.th>
2026-02-16 10:11:49 +0700

    3

--- src/practicals/p03/product.ts
+++ src/practicals/p03/product.ts
@@ -1,2 +1,21 @@
-export class Product {}
+export class Product {
+    name: string;
+    private price: number;
+    static DISCOUNT_PERCENT: number = 10;
+
+    constructor() {
+        this.name = ''
+        this.price = 0
+    }
+
+    getPrice(): number {
+        return this.price;
+    }
+    setPrice(price: number): void {
+        this.price = price;
+    }
+    getProfile(): string {
+        return this.name;
+    }
+}
 
